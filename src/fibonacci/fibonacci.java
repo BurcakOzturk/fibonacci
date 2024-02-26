5d206aae7de790436128a65ed2d15078e3334fde
Burçak ÖZTÜRK <159647466+BurcakOzturk@users.noreply.github.com>
2024-02-26 11:27:50 +0300

    ilk yayim

---
+++ src/fibonacci/fibonacci.java
@@ -0,0 +1,16 @@
+package fibonacci;
+
+public class fibonacci {
+    public static void main(String[] args) {
+        int fibo=0, fiboTut=1,toplam;
+
+        for (int i = 1; i <= 8; ++i) {
+            System.out.print(fibo + " ");
+            toplam = fibo + fiboTut;
+            System.out.print("+ " + fiboTut + " = " + toplam + "\n");
+
+            fibo = fiboTut;
+            fiboTut = toplam;
+        }
+    }
+}
